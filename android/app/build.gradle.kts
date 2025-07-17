035ea9e9fc1d036f527487ec32f6d7b8413706b4
harshcode2109 <156992122+harshcode2109@users.noreply.github.com>
2025-07-17 09:51:43 +0530

    commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,63 @@
+ plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.todo_flutter"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.todo_flutter"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+ dependencies {
+     // Firebase BoM (manages versions automatically)
+     implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+
+     // Firebase services for your Todo app
+     implementation("com.google.firebase:firebase-auth")
+     implementation("com.google.firebase:firebase-firestore")
+     implementation("com.google.firebase:firebase-analytics") // optional
+
+     // Recommended AndroidX libraries
+     implementation("androidx.core:core-ktx:1.12.0")
+     implementation("androidx.appcompat:appcompat:1.6.1")
+     implementation("com.google.android.material:material:1.9.0")
+ }
+
+
+
+// ✅ Add this at the very end of this file
+ apply(plugin = "com.google.gms.google-services")
---
+++ lib/screens/authform.dart
@@ -0,0 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey=GlobalKey<FormState>();
+  var _email='';
+  var _password='';
+  var _username='';
+  bool _isLogin=true;
+
+  startauthentication() async{
+    final isvalid=_formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if(isvalid){
+      _formKey.currentState!.save();
+      submitform(_email,_password,_username);
+    }
+  }
+  submitform(String email, String password, String username) async{
+    final auth=FirebaseAuth.instance;
+    UserCredential authResult;
+
+    try{
+      if(_isLogin){
+        authResult=await auth.signInWithEmailAndPassword(email: email, password: password );
+
+      }
+      else{
+        authResult= await auth.createUserWithEmailAndPassword(email: email, password: password  );
+        String uid= authResult.user!.uid;
+        await FirebaseFirestore.instance.collection('users').doc(uid).set({
+          'username': username,
+          'email': email,
+        });
+      }
+
+    }
+    catch(error){
+      print(error);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: ListView(
+        children: <Widget> [
+           Padding(padding: EdgeInsets.all(10.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                validator: (value){
+                  if(value!.isEmpty|| !value.contains('@')){
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  _email=value!;
+                },
+                keyboardType: TextInputType.emailAddress,
+                key: ValueKey('email'),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  labelStyle: GoogleFonts.roboto(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  )
+                ),
+              )
+            ],
+
+
+    ),)
+           ),
+
+
+            ],
+          ),
+
+
+    );
+  }
+}
